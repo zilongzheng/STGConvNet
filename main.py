ddc5e90751c113ea79e369b94f448b141f0b03bc
Zilong Zheng <zilongzheng0318@gmail.com>
2018-03-02 15:38:21 -0800

    update with fast langevin

--- main.py
+++ main.py
@@ -2,36 +2,43 @@ import argparse
 import tensorflow as tf
 from src.model import STGConvnet
 
+FLAGS = tf.app.flags.FLAGS
+
+# model hyper-parameters
+tf.flags.DEFINE_integer('image_size', 224, 'Image size to rescale images')
+tf.flags.DEFINE_integer('batch_size', 1, 'Batch size of training images')
+tf.flags.DEFINE_integer('num_chain', 3, 'Number of synthesized videos for each batch')
+tf.flags.DEFINE_integer('num_epochs', 500, 'Number of epochs to train')
+tf.flags.DEFINE_integer('num_frames', 70, 'Number of frames for each training video')
+
+# parameters for descriptorNet
+tf.flags.DEFINE_float('lr', 0.001, 'Initial learning rate for descriptorNet')
+tf.flags.DEFINE_float('beta1', 0.5, 'Momentum term of adam')
+tf.flags.DEFINE_float('refsig', 1, 'Standard deviation for reference distribution')
+tf.flags.DEFINE_integer('sample_steps', 20, 'Sample steps for Langevin dynamics')
+tf.flags.DEFINE_float('step_size', 0.3, 'Step size for descriptor Langevin dynamics')
+
+# misc
+tf.flags.DEFINE_string('data_dir', './trainingVideo', 'The data directory')
+tf.flags.DEFINE_string('category', 'fire_pot', 'The name of dataset')
+tf.flags.DEFINE_string('output_dir', './output', 'The output directory for saving results')
+tf.flags.DEFINE_integer('log_step', 10, 'Number of epochs to save output results')
+tf.flags.DEFINE_boolean('test', False, 'True if in testing mode')
+tf.flags.DEFINE_string('ckpt', None, 'Checkpoint path to load')
+tf.flags.DEFINE_integer('sample_size', 100, 'Number of images to generate during test.')
+
+
 def main():
-    parser = argparse.ArgumentParser()
-
-    # model hyper-parameters
-    parser.add_argument('-sz', '--image_size', type=int, default=224)
-
-    # training hyper-parameters
-    parser.add_argument('-num_epochs', type=int, default=500)
-    parser.add_argument('-batch_size', type=int, default=1)
-    parser.add_argument('-num_chain', type=int, default=3, help='number of synthesized results for each batch of training data')
-    parser.add_argument('-num_frames', type=int, default=70, help='number of frames used in training data')
-    parser.add_argument('-lr', type=float, default=0.001, help='learning rate')
-    parser.add_argument('-beta1', type=float, default=0.5, help='momentum1 in Adam')
-
-    # langevin hyper-parameters
-    parser.add_argument('-delta', '--step_size', type=float, default=0.3)
-    parser.add_argument('-sample_steps', type=int, default=20)
-
-    # misc
-    parser.add_argument('-output_dir', type=str, default='./output', help='output directory')
-    parser.add_argument('-category', type=str, default='fire_pot')
-    parser.add_argument('-data_path', type=str,
-                        default='./trainingVideo/', help='root directory of data')
-    parser.add_argument('-log_step', type=int, default=10, help='number of steps to output synthesized image')
-
-    opt = parser.parse_args()
+    model = STGConvnet(
+        net_type= 'FC_S_large',
+        num_epochs=FLAGS.num_epochs, image_size=FLAGS.image_size, num_frames=FLAGS.num_frames,
+        batch_size=FLAGS.batch_size, num_chain=FLAGS.num_chain, lr=FLAGS.lr, beta1= FLAGS.beta1,
+        refsig=FLAGS.refsig, sample_steps= FLAGS.sample_steps, step_size= FLAGS.step_size,
+        data_path=FLAGS.data_dir, category=FLAGS.category, output_dir= FLAGS.output_dir, log_step=FLAGS.log_step
+    )
 
     with tf.Session() as sess:
-        model = STGConvnet(sess, opt)
-        model.train()
+        model.train(sess)
 
 if __name__ == '__main__':
     main()
--- src/model.py
+++ src/model.py
@@ -7,24 +7,27 @@ from ops import *
 from util import *
 from progressbar import ETA, Bar, Percentage, ProgressBar
 
+
 class STGConvnet(object):
-    def __init__(self, sess, config):
-        self.sess = sess
-        self.net_type = 'FC_S_large'
-        self.batch_size = config.batch_size
-        self.image_size = config.image_size
-        self.num_frames = config.num_frames
-        self.num_chain = config.num_chain
-        self.num_epochs = config.num_epochs
+    def __init__(self, net_type='FC_S_large', batch_size=1, image_size=224, num_frames=70, num_chain=3, num_epochs=1000,
+                 lr=0.01, beta1=0.5, refsig=0.1, step_size=0.3, sample_steps=20,
+                 data_path='/tmp/data', category='fire_pot', log_step=10, output_dir='./output'):
+        self.net_type = net_type
+        self.batch_size = batch_size
+        self.image_size = image_size
+        self.num_frames = num_frames
+        self.num_chain = num_chain
+        self.num_epochs = num_epochs
 
-        self.lr = config.lr
-        self.beta1 = config.beta1
-        self.step_size = config.step_size
-        self.sample_steps = config.sample_steps
+        self.lr = lr
+        self.beta1 = beta1
+        self.step_size = step_size
+        self.refsig = refsig
+        self.sample_steps = sample_steps
 
-        self.data_path = os.path.join(config.data_path, config.category)
-        self.log_step = config.log_step
-        self.output_dir = os.path.join(config.output_dir, config.category)
+        self.data_path = os.path.join(data_path, category)
+        self.log_step = log_step
+        self.output_dir = os.path.join(output_dir, category)
 
         self.log_dir = os.path.join(self.output_dir, 'log')
         self.train_dir = os.path.join(self.output_dir, 'observed_sequence')
@@ -36,9 +39,9 @@ class STGConvnet(object):
             tf.gfile.DeleteRecursively(self.log_dir)
         tf.gfile.MakeDirs(self.log_dir)
 
-        self.syn = tf.placeholder(shape=[self.num_chain, self.num_frames, self.image_size, self.image_size, 3], dtype=tf.float32)
+        self.syn = tf.placeholder(shape=[self.num_chain, self.num_frames, self.image_size, self.image_size, 3],
+                                  dtype=tf.float32)
         self.obs = tf.placeholder(shape=[None, self.num_frames, self.image_size, self.image_size, 3], dtype=tf.float32)
-
 
     def descriptor(self, inputs, reuse=False):
         with tf.variable_scope('des', reuse=reuse):
@@ -88,25 +91,34 @@ class STGConvnet(object):
             else:
                 return NotImplementedError
 
-    def langevin_dynamics(self, samples, gradient, batch_id):
-        for i in range(self.sample_steps):
-            noise = np.random.randn(*samples.shape)
-            grad = self.sess.run(gradient, feed_dict = {self.syn: samples})
-            samples = samples - 0.5 * self.step_size * self.step_size * (samples - grad) + self.step_size * noise
-            self.pbar.update(batch_id * self.sample_steps + i)
-        return samples
+    def langevin_dynamics(self, syn_arg):
+        def cond(i, syn):
+            return tf.less(i, self.sample_steps)
 
-    def train(self):
+        def body(i, syn):
+            noise = tf.random_normal(shape=tf.shape(syn), name='noise')
+            syn_res = self.descriptor(syn, reuse=True)
+            grad = tf.gradients(syn_res, syn, name='grad_des')[0]
+            syn = syn - 0.5 * self.step_size * self.step_size * (syn / self.refsig / self.refsig - grad)
+            syn = syn + self.step_size * noise
+            return tf.add(i, 1), syn
+
+        with tf.name_scope("langevin_dynamics"):
+            i = tf.constant(0)
+            i, syn = tf.while_loop(cond, body, [i, syn_arg])
+            return syn
+
+    def train(self, sess):
 
         obs_res = self.descriptor(self.obs, reuse=False)
         syn_res = self.descriptor(self.syn, reuse=True)
-        train_loss = tf.subtract(tf.reduce_mean(syn_res,axis=0), tf.reduce_mean(obs_res,axis=0))
+        train_loss = tf.subtract(tf.reduce_mean(syn_res, axis=0), tf.reduce_mean(obs_res, axis=0))
         train_loss_mean, train_loss_update = tf.contrib.metrics.streaming_mean(train_loss)
 
         recon_err_mean, recon_err_update = tf.contrib.metrics.streaming_mean_squared_error(
-            tf.reduce_mean(self.syn,axis=0),tf.reduce_mean(self.obs,axis=0))
+            tf.reduce_mean(self.syn, axis=0), tf.reduce_mean(self.obs, axis=0))
 
-        dLdI = tf.gradients(syn_res, self.syn)[0]
+        langevin_descriptor = self.langevin_dynamics(self.syn)
 
         # Prepare training data
         loadVideoToFrames(self.data_path, self.train_dir)
@@ -126,49 +138,56 @@ class STGConvnet(object):
         update_grads = [accum_vars[i].assign_add(gv[0]) for i, gv in enumerate(grads_and_vars)]
         des_grads = [tf.reduce_mean(tf.abs(grad)) for (grad, var) in grads_and_vars if '/w' in var.name]
         # update by mean of gradients
-        apply_grads = optimizer.apply_gradients([(tf.divide(accum_vars[i], num_batches), gv[1]) for i, gv in enumerate(grads_and_vars)])
-
-
-        self.sess.run(tf.global_variables_initializer())
-        self.sess.run(tf.local_variables_initializer())
-
-        sample_size = self.num_chain * num_batches
-        sample_video = np.random.randn(sample_size, self.num_frames, self.image_size, self.image_size, 3)
+        apply_grads = optimizer.apply_gradients(
+            [(tf.divide(accum_vars[i], num_batches), gv[1]) for i, gv in enumerate(grads_and_vars)])
 
         tf.summary.scalar('train_loss', train_loss_mean)
         tf.summary.scalar('recon_err', recon_err_mean)
         summary_op = tf.summary.merge_all()
 
-        saver = tf.train.Saver(max_to_keep=50)
-        writer = tf.summary.FileWriter(self.log_dir, self.sess.graph)
+        sess.run(tf.global_variables_initializer())
+        sess.run(tf.local_variables_initializer())
 
-        for epoch in range(self.num_epochs):
+
+        sample_size = self.num_chain * num_batches
+        sample_video = np.random.randn(sample_size, self.num_frames, self.image_size, self.image_size, 3)
+
+        saver = tf.train.Saver(max_to_keep=50)
+        writer = tf.summary.FileWriter(self.log_dir, sess.graph)
+
+        # make graph immutable
+        tf.get_default_graph().finalize()
+
+        # store graph in protobuf
+        if not os.path.exists(self.model_dir):
+            os.makedirs(self.model_dir)
+        with open(self.model_dir + '/graph.proto', 'w') as f:
+            f.write(str(tf.get_default_graph().as_graph_def()))
+
+        for epoch in xrange(self.num_epochs):
 
             gradients = []
 
-            widgets = ["Epoch #%d|" % epoch, Percentage(), Bar(), ETA()]
-            self.pbar = ProgressBar(maxval=num_batches * self.sample_steps, widgets=widgets)
-            self.pbar.start()
+            sess.run(reset_grads)
+            for i in xrange(num_batches):
+                obs_data = train_data[i * self.batch_size:min(len(train_data), (i + 1) * self.batch_size)]
+                syn = sample_video[i * self.num_chain:(i + 1) * self.num_chain]
 
-            self.sess.run(reset_grads)
-            for i in range(num_batches):
-                obs_data = train_data[i * self.batch_size:min(len(train_data), (i+1) * self.batch_size)]
-                syn = sample_video[i * self.num_chain:(i+1) * self.num_chain]
+                syn = sess.run(langevin_descriptor, feed_dict={self.syn: syn})
 
-                syn = self.langevin_dynamics(syn, dLdI, i)
+                grad = sess.run([des_grads, update_grads, train_loss_update],
+                                     feed_dict={self.obs: obs_data, self.syn: syn})[0]
 
-                grad = self.sess.run([des_grads, update_grads, train_loss_update], feed_dict={self.obs: obs_data, self.syn: syn})[0]
-
-                self.sess.run(recon_err_update, feed_dict={self.obs: obs_data, self.syn: syn})
+                sess.run(recon_err_update, feed_dict={self.obs: obs_data, self.syn: syn})
 
                 sample_video[i * self.num_chain:(i + 1) * self.num_chain] = syn
 
-                gradients.append(np.mean(grad))
-            self.pbar.finish()
+                gradients.append(grad)
 
-            self.sess.run(apply_grads)
-            [loss, recon_err, summary] = self.sess.run([train_loss_mean, recon_err_mean, summary_op])
-            print('Epoch #%d, descriptor loss: %.4f, SSD weight: %4.4f, Avg MSE: %4.4f' % (epoch, loss, float(np.mean(gradients)), recon_err))
+            sess.run(apply_grads)
+            [loss, recon_err, summary] = sess.run([train_loss_mean, recon_err_mean, summary_op])
+            print('Epoch #%d, descriptor loss: %.4f, SSD weight: %4.4f, Avg MSE: %4.4f' % (
+            epoch, loss, float(np.mean(gradients)), recon_err))
             writer.add_summary(summary, epoch)
 
             if epoch % self.log_step == 0:
@@ -176,10 +195,8 @@ class STGConvnet(object):
                     os.makedirs(self.sample_dir)
                 saveSampleSequence(sample_video + img_mean, self.sample_dir, epoch, col_num=10)
 
-                if not os.path.exists(self.model_dir):
-                    os.makedirs(self.model_dir)
-                saver.save(self.sess, "%s/%s" % (self.model_dir, 'model.ckpt'), global_step=epoch)
+                saver.save(sess, "%s/%s" % (self.model_dir, 'model.ckpt'), global_step=epoch)
 
             if epoch % 20 == 0:
-                saveSampleVideo(sample_video + img_mean, self.result_dir, original=(train_data + img_mean), global_step=epoch)
-
+                saveSampleVideo(sample_video + img_mean, self.result_dir, original=(train_data + img_mean),
+                                global_step=epoch)
